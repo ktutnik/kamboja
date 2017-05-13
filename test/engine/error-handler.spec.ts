c6fa1e9c116e03319775247eaa859127b49c79b2
Ketut Sandiarsa <ktutnik@gmail.com>
2017-05-13 17:17:00 +0800

    Add error handler

--- test/engine/error-handler.spec.ts
+++ test/engine/error-handler.spec.ts
@@ -50,11 +50,35 @@ describe("ErrorHandler", () => {
         Chai.expect(status).eq(500)
     })
 
-    it("Should render error view on Controller error", () => {
+    it("Should return json on ApiController error", () => {
         let meta = H.fromFile("controller/api-controller.js", new Resolver.DefaultPathResolver(__dirname))
         let infos = Transformer.transform(meta)
         let info = infos.filter(x => x.methodMetaData.name == "returnTheParam")[0]
         let error = { status: 500, message: "Fatal error", routeInfo: info }
+        let engine = new Engine.ErrorHandler(error, { rootPath: __dirname }, httpRequest, httpResponse)
+        engine.execute()
+        let body = responseMock.json.getCall(0).args[0]
+        let status = responseMock.status.getCall(0).args[0]
+        Chai.expect(body).eq("Fatal error")
+        Chai.expect(status).eq(500)
+    })
+
+    it("Should return xml when request content type is xml and no routeInfo specified", () => {
+        let error = { status: 500, message: "Fatal error" }
+        requestMock.getHeader.withArgs("Content-Type").returns("text/xml")
+        let engine = new Engine.ErrorHandler(error, { rootPath: __dirname }, httpRequest, httpResponse)
+        engine.execute()
+        let contentType = responseMock.setContentType.getCall(0).args[0]
+        let body = responseMock.send.getCall(0).args[0]
+        let status = responseMock.status.getCall(0).args[0]
+        Chai.expect(contentType).eq("text/xml")
+        Chai.expect(body).eq("Fatal error")
+        Chai.expect(status).eq(500)
+    })
+
+    it("Should return json when request content type is application/json and no routeInfo specified", () => {
+        let error = { status: 500, message: "Fatal error" }
+        requestMock.getHeader.withArgs("Content-Type").returns("application/json")
         let engine = new Engine.ErrorHandler(error, { rootPath: __dirname }, httpRequest, httpResponse)
         engine.execute()
         let body = responseMock.json.getCall(0).args[0]
