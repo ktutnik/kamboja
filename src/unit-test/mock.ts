38d01397d3032916a1d13dbb4941b66af78cb0d0
Ketut Sandiarsa <ktutnik@gmail.com>
2017-05-22 16:49:54 +0800

    Fixing mock error

--- src/unit-test/mock.ts
+++ src/unit-test/mock.ts
@@ -7,7 +7,7 @@ export interface Mockable<T, TResult>{
     MOCKS:Mock<T, TResult>
 }
 
-export function mock<T, TResult>(obj:T, sinon: (obj, key:string) => TResult) : (T & Mockable<T, TResult>){
+export function mock<T, TResult>(obj:T, sinon: (obj:any, key:string) => TResult) : (T & Mockable<T, TResult>){
     let mocks:Mock<T, TResult> = <any> {}
     for(let key in obj){
         mocks[key] = sinon(obj, key)
