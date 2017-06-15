8558ffe7f430df03b4b742b848c0dfc9f59ffb44
I Ketut Sandiarsa <ktutnik@gmail.com>
2017-06-15 17:59:00 +0800

    Switch back to ES5 for better support on node 4 and 5

--- packages/kamboja/src/controller/errors.ts
+++ packages/kamboja/src/controller/errors.ts
@@ -1,12 +1,15 @@
 import * as Tslib from "tslib"
 
+/*
 export class HttpStatusError extends Error {
     constructor(public status:number, message?:string){
         super(message)
+        Object.setPrototypeOf(this, HttpStatusError.prototype);
     }
 }
+*/
 
-/*
+
 export class HttpStatusError {
     constructor(public status:number, public message?:string){
         var err = Error.call(this, message);
@@ -17,5 +20,4 @@ export class HttpStatusError {
 }
 Tslib.__extends(HttpStatusError, Error)
 
-*/
 
